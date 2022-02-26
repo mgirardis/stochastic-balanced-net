2995bf89830fcef76cdc069e2887781296de5a97
mgirardis <girardi.s@gmail.com>
2022-02-26 16:05:12 -0500

    fixing dir

--- run/run_sbatch_local.sh
+++ run/run_sbatch_local.sh
@@ -7,6 +7,12 @@
 #SBATCH -N 1-3
 #SBATCH --array=2-2       # indices of the lines from lines_to_run.txt to be executed
 
+# going up one directory
+THIS_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+THIS_SCRIPT_DIR=$(dirname "$THIS_SCRIPT_DIR")
+
+cd $THIS_SCRIPT_DIR
+
 LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p lines_to_run.txt)
 
 echo Running task $SLURM_ARRAY_TASK_ID ::: $LINE
